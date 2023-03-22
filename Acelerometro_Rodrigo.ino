5539f041c5251ae59e42fc3e5a12ba8958c8ce33
RodrigoBCGomes <rorigosgtau@gmail.com>
2023-03-22 16:56:38 -0300

    criando variaveis

--- Acelerometro_Rodrigo.ino
+++ Acelerometro_Rodrigo.ino
@@ -1,3 +1,21 @@
+#include <MOU6050_tockn.h>
+#include <Wire.h>
+
+#define pinLedX 12
+#define pinLedY 11
+#define pinLedZ 10
+
+#define <MPU6050_ADDR 0x68>
+
+#define DEBUG
+MPU6050 mpu6050(Wire);
+
+float anguloX;
+float anguloY;
+float anguloZ;
+
+unsigned long controleTempo;
+
 void setup() {
 
 }
